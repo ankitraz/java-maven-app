7129b278734adcca36602a1fe54946649963715c
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-01-13 21:56:28 +0530

    updated script

---
+++ script.groovy
@@ -0,0 +1,20 @@
+def build(){
+    echo "Building the application..."
+    sh 'mvn package'
+}
+
+def Buildimage(){
+    echo "Building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
+        sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
+        sh 'docker build -t ankitraz/myapp:ank-1.0 .'
+        sh 'docker push ankitraz/myapp:ank-1.0'
+    }
+
+def test(){
+    echo "Testing the application..."
+}
+
+def deploy(){
+    echo "Deploying the application..."
+}
