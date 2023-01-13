1c3ed81c3c6584e770f7a0c0eb3de730716e17ee
ankitraz <109873375+ankitraz@users.noreply.github.com>
2023-01-13 22:14:18 +0530

    updated script

--- basic-jenkins-pipeline/script.groovy
+++ basic-jenkins-pipeline/script.groovy
@@ -3,13 +3,14 @@ def build(){
     sh 'mvn package'
 }
 
-def Buildimage(){
+def Buildimage() {
     echo "Building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]){
+    withCredentials([usernamePassword(credentialsId: 'dockerhub-id-pass', usernameVariable: 'DOCKER_USER', passwordVariable: 'DOCKER_PASS')]) {
         sh 'docker login -u $DOCKER_USER -p $DOCKER_PASS'
         sh 'docker build -t ankitraz/myapp:ank-1.0 .'
         sh 'docker push ankitraz/myapp:ank-1.0'
     }
+}
 
 def test(){
     echo "Testing the application..."
@@ -18,5 +19,4 @@ def test(){
 def deploy(){
     echo "Deploying the application..."
 }
-
 return this
